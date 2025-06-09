898ba0d4d3e79c121cecf0cffd15a572bc19aecb
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-09 16:31:30 +0530

    ls1.2

--- Python/1.py
+++ Python/1.py
@@ -1,3 +1,12 @@
 print("Hello, I'm Aayana")
 print("I am a student of 9th grade")
 print("I like to read Novels.")
+
+favourite_novel="Powerless"
+favourite_author="Holly Jackson"
+print("My favourite novel is", favourite_novel )
+print( "My favourite author is", favourite_author )
+
+a= 5 
+b= 7 
+print ( a+b)
