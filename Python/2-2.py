3ac866f2518e762ef8e2dbc7481a7a8c30562553
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-11 12:41:01 +0530

    2-2

---
+++ Python/2-2.py
@@ -0,0 +1,16 @@
+First_name="Aayana"
+Second_name="Saini" 
+
+word= "novels"*5 
+
+print("full name:", First_name + Second_name)
+print("first name:", First_name)
+print("second name:", Second_name)
+print("string is:", word)
+
+dummy="Murder"
+
+print("length of string", len(dummy))
+print("first letter of dummy", dummy[0])
+print("last letter of dummy", dummy[5])
+print("string sliced:", dummy [0:3])
