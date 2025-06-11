0dc7c1779c3e23c3c0205c4b85aca31839b6d9ca
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-11 12:15:00 +0530

    2

---
+++ Python/2.py
@@ -0,0 +1,16 @@
+name="Aayana"
+weight= 48.5
+age= 14
+is_student = True
+
+print("Student's name:", name)
+print("data type is", type (name))
+
+print("Student's weight:", weight)
+print("data type is", type (age) )
+
+print ("Student's age:", age )
+print("data type is", type (age))
+
+print ("is student:", is_student)
+print ("data type is", is_student)
