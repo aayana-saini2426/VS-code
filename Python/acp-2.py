48f965ddefad9f5a35c6a9f650bab2ef0571591e
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-11 12:56:31 +0530

    acp

---
+++ Python/acp-2.py
@@ -0,0 +1,7 @@
+a=int(input("Enter value a"))
+b=int(input("Enter value b"))
+c=int(input("Enter value c"))
+
+print("Before swapping a:", a ,"b:", b ,"c:", c )
+a,b,c= c,b,a 
+print("after swapping a:" , a ,"b:", b ,"c:", c)
