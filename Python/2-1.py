33a8972d40b041dd683002ad3d8fa87c4c5833da
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-11 12:29:32 +0530

    2-1

---
+++ Python/2-1.py
@@ -0,0 +1,29 @@
+num1= int(input("Enter the first number:"))
+num2= int(input("Enter the second number:"))
+
+print("first number is", num1)
+print("second number is", num2)
+
+print("addition", num1+num2)
+print("subtraction", num1-num2)
+print("multiplication", num1*num2)
+print("division", num1/num2)
+print("floor division", num1//num2)
+print("modulus", num1%num2)
+
+#Assignment Operators
+
+num1+=num2 
+print("num1 new value", num1 )
+
+num1-=num2 
+print("num1 new value", num1 )
+
+num1*=num2 
+print("num1 new value", num1 )
+
+num1/=num2 
+print("num1 new value", num1 )
+
+num1//=num2 
+print("num1 new value", num1 )
