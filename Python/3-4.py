12968543f17b34803921c4fc8791e639f68e272c
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-12 11:44:54 +0530

    calendar

---
+++ Python/3-4.py
@@ -0,0 +1,8 @@
+import datetime
+
+current_time= datetime.datetime.now()
+print("time now at greenwich meridian time is", end="")
+print(current_time)
+import calendar
+
+print( "/n", calendar.calendar(2025))
