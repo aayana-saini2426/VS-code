126ad96a46a04e8b47287ab1383505b334d0cde9
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-12 11:37:28 +0530

    3-3

---
+++ Python/3-3.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the number:"))
+if num>= 50:
+    if num%2 ==0:
+        print("the number is even")
+    else: 
+        print("The number is odd")
+else:
+    print("The number is should be greater than 50 ")
