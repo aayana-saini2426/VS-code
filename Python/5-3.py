cbca3a966e2b2de8e8c772a179976df7970ad446
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-06-29 13:11:21 +0530

    tutrle

---
+++ Python/5-3.py
@@ -0,0 +1,7 @@
+number=int(input("Enter the number"))
+print("the table of", number , "is :")
+
+#for loop
+
+for i in range (1,11): 
+    print (number , "X", i, "=" , number*i)
---
+++ Python/6.py
@@ -0,0 +1,10 @@
+import turtle
+turtle. Screen().bgcolor("Blue")
+
+sc=turtle.Screen()
+sc.setup(400,300)
+board=turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i= i+1
