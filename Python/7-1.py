d73f538d0057e50b24b8c0bf62691be2f7dada76
aayana-saini2426 <aayana.saini2011@gmail.com>
2025-07-03 18:02:08 +0530

    7-1

---
+++ Python/7-1.py
@@ -0,0 +1,18 @@
+dict={}
+dict={ 1:"apple", 2: "ball"}
+dict={'name': "john", 1: [2,3,4]}
+
+dict={'name' : "john", 'age': "24"}
+
+print(dict["name"])
+print(dict["age"])
+
+dict['age'] ='27'
+print(dict["age"])
+
+dict['address']= 'downtown'
+
+print(dict["address"])
+
+dict.clear 
+print(dict)
